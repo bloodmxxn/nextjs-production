fb45d445b213cb0049620ebd9ebeb125326f3819
BloodMxxn <bloodm0x0n@gmail.com>
2026-07-25 12:51:25 +0330

    update

--- next.config.ts
+++ next.config.ts
@@ -4,14 +4,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   output: "standalone",
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [{ key: "X-Forwarded-Host", value: "repoti.ir" }],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
